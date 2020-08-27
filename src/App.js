5fdd5f2f858ad28045b8759f4bf98483453c55c7
amanjagdev <amankumarjagdev@gmail.com>
2020-08-27 20:46:59 +0530

    Maded Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-function App() {
+//Importing Pages
+import Home from './pages/Home';
+import Register from './pages/Register';
+
+//Importing Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/register" exact component={Register} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
